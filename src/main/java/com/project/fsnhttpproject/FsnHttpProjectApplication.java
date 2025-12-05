20ed954597e291480a321c0f7bc4a1e9a24d956e
SOOsuhyuncho <chosh0132@khu.ac.kr>
2025-12-05 23:59:08 +0900

    1

---
+++ src/main/java/com/project/fsnhttpproject/FsnHttpProjectApplication.java
@@ -0,0 +1,13 @@
+package com.project.fsnhttpproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsnHttpProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FsnHttpProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/fsnhttpproject/FsnHttpProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.fsnhttpproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FsnHttpProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
